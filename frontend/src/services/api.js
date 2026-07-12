ba130f9fbdae6e92aa8ee28f31126da410b01898
infinty11 <awasthiamit412@gmail.com>
2026-07-12 22:35:16 +0530

    shifted db to aiven

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
+    // baseURL: 'http://localhost:5000/api',
   baseURL: 'https://filpulse.onrender.com/api',
   timeout: 30_000,
   headers: { 'Content-Type': 'application/json' },
