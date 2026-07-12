fd96ddc53883a3a1f90c0ed404232256bd6b0d2f
infinty11 <awasthiamit412@gmail.com>
2026-07-12 22:53:19 +0530

    add sll in db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,20 @@
 // backend/config/database.js
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'finpulse_db',
+ 
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "ca.pem")),
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
